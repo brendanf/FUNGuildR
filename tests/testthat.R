e691ed9f71968eb42d0d39ac6a21c369044201c3
brendanf <brendan.furneaux@gmail.com>
2020-09-25 17:13:40 +0200

    add unit tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(FUNGuildR)
+
+test_check("FUNGuildR")
---
+++ tests/testthat/test-funguild_assign.R
@@ -0,0 +1,62 @@
+test_that("nemaguild_assign does not regress", {
+  expect_known_value(
+    nemaguild_assign(sample_nema, db = nemaguild_testdb),
+    file = "nemaguild",
+    update = FALSE
+  )
+})
+
+test_that("funguild_assign does not regress", {
+  expect_known_value(
+    funguild_assign(sample_fungi, db = funguild_testdb),
+    file = "funguild",
+    update = FALSE
+  )
+})
+
+test_that("funguild_assign accepts a character vector", {
+  expect_known_value(
+    funguild_assign(sample_fungi$Taxonomy, db = funguild_testdb),
+    file = "funguild_char",
+    update = FALSE
+  )
+})
+
+sample2 <- sample_fungi
+sample2$Taxonomy <- chartr(";", ",", sample2$Taxonomy)
+test_that("funguild_assign accepts a comma delimited tax column", {
+  expect_known_value(
+    funguild_assign(sample2, db = funguild_testdb),
+    file = "funguild_comma",
+    update = FALSE
+  )
+})
+
+sample2$Taxonomy <- chartr(",", ":", sample2$Taxonomy)
+test_that("funguild_assign accepts a comma delimited tax column", {
+  expect_known_value(
+    funguild_assign(sample2, db = funguild_testdb),
+    file = "funguild_colon",
+    update = FALSE
+  )
+})
+
+names(sample2)[3] <- "classification"
+test_that("funguild_assign accepts a nonstandard taxonomy column", {
+  expect_known_value(
+    funguild_assign(sample2, db = funguild_testdb, tax_col = "classification"),
+    file = "funguild_tax_col",
+    update = FALSE
+  )
+})
+
+
+
+names(sample2)[3] <- "classification"
+test_that("funguild_assign accepts a nonstandard taxonomy column in a character", {
+  expect_known_value(
+    funguild_assign(sample_fungi$Taxonomy, db = funguild_testdb, tax_col = "tax"),
+    file = "funguild_tax_col_char",
+    update = FALSE
+  )
+})
